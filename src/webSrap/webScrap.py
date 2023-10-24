4b53c82e41f98202dbf05364564d50ea1b76f9b7
ThomasByr <tbouyer2000@gmail.com>
2023-10-24 13:41:24 +0200

    add cli

--- src/webSrap/webScrap.py
+++ src/webSrap/webScrap.py
@@ -2,14 +2,14 @@ import argparse
 
 # import scrapy
 from scrapy.crawler import CrawlerProcess
-from batterie.batterie.spiders import bat_spider
+from .batterie.batterie.spiders import bat_spider
 
 # python webScrap.py --output data.json
 
 
-def run_spider(_):
+def run_spider(output_filename: str):
     process = CrawlerProcess(
-        settings={"FEED_FORMAT": "json", "FEED_URI": "output.json"}
+        settings={"FEED_FORMAT": "json", "FEED_URI": output_filename}
     )
     process.crawl(bat_spider.bat_spider)
     process.start()
